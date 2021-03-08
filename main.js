ea84d1e5dd149d42bc6c1d78a39e273d618e82d4
amaurisrivas <78513366+amaurisrivas@users.noreply.github.com>
2021-03-08 13:53:00 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+//crear un reloj
+var myVar = setInterval(myTimer, 1000);
+     function myTimer() {
+     var d = new Date();
+     var t = d.toLocaleTimeString();
+     document.getElementById("archivo").innerHTML = t;
+}
+function dograce(){
+    var vid = document.getElementById("race"); 
+    let num = Math.floor(Math.random() * (360 - 1)) + 1;
+    let str = num.toString().padStart(3, "0")
+    let archivo = "./dog6/rb6d_" + str + ".mp4";
+    document.getElementById("carrera").innerHTML = archivo;
+    vid.setAttribute('src', archivo);	      
+    vid.autoplay = true;
+    vid.load();
+    vid.play();
+};
+
+setInterval(function() {
+   dograce();
+},50000);
